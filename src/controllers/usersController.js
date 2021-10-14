7fff300301aa07883854d894a8222e9a66184173
LucasRavalle <ravallelucas@gmail.com>
2021-10-14 00:23:23 -0300

    uso de express-validator en register

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,8 +1,48 @@
+const fs = require('fs')
+const path = require('path')
+const {validationResult} = require('express-validator')
+
 module.exports = {
     login: (req, res) => {
         res.render('user/login');
       },
     register: (req, res) => {
         res.render('user/register');
-      }
+      },
+    processRegister: (req, res) => {
+
+        const errors = validationResult(req)
+
+        if (errors.isEmpty()) {
+            const ruta = path.join(__dirname,'..','data','users.json')
+        
+            const usuariosRegistrados = fs.readFileSync(ruta,'utf-8')
+            let usuarios
+        
+            if (usuariosRegistrados === '') {
+                usuarios = []
+            } else {
+                usuarios = JSON.parse(usuariosRegistrados)
+            }
+        
+            const usuario = {
+                nombre: req.body.nombre,
+                email: req.body.email,
+                contraseña: req.body.password,
+                pais: req.body.pais 
+            }
+        
+            usuarios.push(usuario)
+        
+            fs.writeFileSync(ruta, JSON.stringify(usuarios, null, 2))
+        
+            res.redirect('/')
+
+        } else {
+          res.render('user/register', {errors: errors.mapped(), old: req.body})
+        }
+      
+        
+    
+    }
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,38 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {login, register} = require('../controllers/usersController')
-const fs = require('fs')
-const path = require('path')
+const {login, register, processRegister} = require('../controllers/usersController')
+const validate= require('../middlewares/validateRegister')
 
 /* GET users listing. */
 
 // CREATE USER
 
 router.get('/register', register)
-router.post('/register', (req, res) => {
-    const ruta = path.join(__dirname,'..','data','users.json')
-
-    const usuariosRegistrados = fs.readFileSync(ruta,'utf-8')
-    let usuarios
-
-    if (usuariosRegistrados === '') {
-        usuarios = []
-    } else {
-        usuarios = JSON.parse(usuariosRegistrados)
-    }
-
-    const usuario = {
-        nombre: req.body.nombre,
-        email: req.body.email
-    }
-
-    usuarios.push(usuario)
-
-    fs.writeFileSync(ruta, JSON.stringify(usuarios, null, 2))
-
-    res.redirect('/users/register')
-
-} )
+router.post('/register', validate, processRegister)
 
 
 //LOGIN USER
